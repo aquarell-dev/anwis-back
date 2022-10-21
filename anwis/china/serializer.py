21ac6895321dbe65d3e4b35a7ad6cbb4ae76ca92
Aquarell <aquarell.dev@gmail.com>
2022-10-21 12:39:33 +0300

    serializers

---
+++ anwis/china/serializer.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from drf_writable_nested import WritableNestedModelSerializer
+
+from .models import ChinaDistributor, Product, OrderForProject, Order, Status, IndividualEntrepreneur
+
+
+class ChinaSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = ChinaDistributor
+
+
+class OrderForProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = OrderForProject
+
+
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Status
+
+
+class IndividualEntrepreneurSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = IndividualEntrepreneur
+
+
+class OrderListRetrieveSerializer(serializers.ModelSerializer):
+    individual_entrepreneur = IndividualEntrepreneurSerializer()
+    china_distributor = ChinaSerializer()
+    order_for_project = OrderForProjectSerializer()
+    status = StatusSerializer()
+
+    class Meta:
+        exclude = ('draft',)
+        model = Order
+
+
+class OrderCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        exclude = ('draft',)
+        model = Order
