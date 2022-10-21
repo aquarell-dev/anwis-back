38a9ec99c325ca1d67962defdd9b93fd5aabefe8
Aquarell <aquarell.dev@gmail.com>
2022-10-21 12:38:35 +0300

    china urls

--- anwis/anwis/urls.py
+++ anwis/anwis/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView, TokenVerifyView,
@@ -7,11 +7,13 @@ from rest_framework_simplejwt.views import (
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/', include('china.urls')),
 ]
 
 if settings.DEBUG:
