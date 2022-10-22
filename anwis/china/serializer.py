c56899c0d464abdf173ad41e1ad59a31c22a9977
Aquarell <aquarell.dev@gmail.com>
2022-10-22 14:30:02 +0300

    fixed error

--- anwis/china/serializer.py
+++ anwis/china/serializer.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from drf_writable_nested import WritableNestedModelSerializer
 
 from .models import ChinaDistributor, Product, OrderForProject, Order, Status, IndividualEntrepreneur
 
