162ce8b35d067d1fb710f42013e6ee2702419254
Aquarell <aquarell.dev@gmail.com>
2022-11-01 00:35:55 +0300

    included leftover ulrs

--- anwis/anwis/urls.py
+++ anwis/anwis/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/', include('china.urls')),
+    path('api/', include('leftover.urls')),
 ]
 
 if settings.DEBUG:
