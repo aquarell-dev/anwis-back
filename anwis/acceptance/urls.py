930dff237e34a459f74923112214c97e96b86143
Aquarell <aquarell.dev@gmail.com>
2022-11-12 21:13:57 +0300

    added new route

--- anwis/acceptance/urls.py
+++ anwis/acceptance/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from acceptance.views import AcceptanceListCreateView
+from acceptance.views import AcceptanceListCreateView, AcceptanceRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('acceptances/', AcceptanceListCreateView.as_view()),
+    path('acceptances/<int:pk>/', AcceptanceRetrieveUpdateDestroyView.as_view()),
 ]
--- anwis/acceptance/views.py
+++ anwis/acceptance/views.py
@@ -7,3 +7,8 @@ from acceptance.serializers import AcceptanceListSerializer
 class AcceptanceListCreateView(generics.ListCreateAPIView):
     queryset = Acceptance.objects.all()
     serializer_class = AcceptanceListSerializer
+
+
+class AcceptanceRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Acceptance.objects.all()
+    serializer_class = AcceptanceListSerializer
