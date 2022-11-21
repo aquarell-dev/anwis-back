c39227ba5b7f0d9f2a898ca217d4569fc8bcc719
Aquarell <aquarell.dev@gmail.com>
2022-11-21 12:14:10 +0300

    added business logic

--- anwis/acceptance/service.py
+++ anwis/acceptance/service.py
@@ -1,30 +1,44 @@
-from acceptance.models import Acceptance, Product, ProductSpecification
+import os
+from datetime import datetime
+
+from acceptance.models import Acceptance, Product, ProductSpecification, AcceptanceCategory
 from china.models import Order, ProductInfo
 
+from blabel import LabelWriter
 
-def does_product_exist(**kwargs):
+from common.services import fetch_leftovers, save_to_default_storage
+
+from django.core.files import File
+
+from documents.models import Document
+
+
+def _does_russian_product_exist(**kwargs):
     try:
         return Product.objects.get(**kwargs)
     except Product.DoesNotExist:
         return None
 
 
+def _does_acceptance_exist(**kwargs):
+    try:
+        return Acceptance.objects.get(**kwargs)
+    except Product.DoesNotExist:
+        return None
+
+
 def _calculate_self_cost(info: ProductInfo, order: Order):
-    delivery = order.dollar_to_rub * (order.real_total_delivery + order.dollar_to_rub)
-    cost_wout_delivery = info.price_rub + info.additional_expenses
+    delivery = ((order.dollar_to_rub * order.real_total_delivery) + order.delivery_expenses) / order.total_quantity
+    cost_wout_delivery = info.price_rub + info.additional_expenses  # = 111
     selfcost = delivery + float(cost_wout_delivery)
-    return selfcost
+    return round(selfcost)
 
 
-def _get_or_create_specification_by_product_id(product: Product, cost, quantity):
+def _get_specification_by_product_id(product: Product):
     try:
         return ProductSpecification.objects.get(product_id=product.id)
     except Product.DoesNotExist:
-        return ProductSpecification.objects.create(
-            product=product,
-            cost=cost,
-            quantity=quantity
-        )
+        return None
 
 
 def _create_new_product(info: ProductInfo, order: Order) -> tuple:
@@ -39,10 +53,58 @@ def _create_new_product(info: ProductInfo, order: Order) -> tuple:
         color=info.product.color,
         brand=info.product.brand,
         last_cost=selfcost,
-        title=info.product.title
+        title=info.product.title,
+        category=AcceptanceCategory.objects.get_or_create(category=info.product.category.category)[0]
     )
 
     return russian_product, selfcost, info.quantity
+
+
+def _patch_or_add_missing_products(order: Order, acceptance: Acceptance):
+    for specification in order.products.all():
+        product = _does_russian_product_exist(
+            linked_china_product_article=specification.product.article,
+            linked_china_product_size=specification.product.size,
+        )
+
+        if product:
+            product.last_cost = _calculate_self_cost(specification, order)
+            category = specification.product.category
+            if category:
+                print(category)
+                product.category = AcceptanceCategory.objects.get_or_create(category=category.category)[0]
+            product.save()
+
+            new_specification = _get_specification_by_product_id(product)
+
+            selfcost = _calculate_self_cost(specification, order)
+
+            if not new_specification:
+                acceptance.products.add(
+                    ProductSpecification.objects.create(
+                        product=product,
+                        cost=selfcost,
+                        quantity=specification.quantity
+                    )
+                )
+            else:
+                new_specification.quantity = specification.quantity
+                new_specification.cost = selfcost
+                new_specification.save()
+                already_existing_objs = [spec.id for spec in acceptance.products.all()]
+                acceptance.products.add(*already_existing_objs, new_specification.id)
+
+            continue
+
+        product, selfcost, quantity = _create_new_product(specification, order)
+
+        acceptance.products.create(
+            cost=selfcost,
+            quantity=quantity,
+            product=product
+        )
+
+    acceptance.save()
 
 
 def create_acceptance_from_order(order: Order):
@@ -58,33 +120,7 @@ def create_acceptance_from_order(order: Order):
         from_order=order.id
     )
 
-    unique_data = [(product_specification, product_specification.product.article, product_specification.product.size)
-                   for product_specification in order.products.all()]
-
-    for specification, article, size in unique_data:
-        product = does_product_exist(linked_china_product_article=article, linked_china_product_size=size)
-        if product:
-            product.last_cost = _calculate_self_cost(specification, order)
-            product.save()
-
-            acceptance.products.add(
-                _get_or_create_specification_by_product_id(
-                    product,
-                    _calculate_self_cost(specification, order),
-                    specification.quantity
-                )
-            )
-
-            continue
-
-        product, selfcost, quantity = _create_new_product(specification, order)
-        acceptance.products.create(
-            cost=selfcost,
-            quantity=quantity,
-            product=product
-        )
-
-    acceptance.save()
+    _patch_or_add_missing_products(order, acceptance)
 
     return acceptance
 
@@ -96,4 +132,84 @@ def update_acceptance_from_order(acceptance: Acceptance, order: Order):
     acceptance.arrived_in_moscow = order.real_in_moscow_date
     acceptance.shipped_from_china = order.shipping_from_china_date
 
+    _patch_or_add_missing_products(order, acceptance)
+
     return acceptance
+
+
+def create_label(data: dict):
+    label_writer = LabelWriter(
+        os.path.join(os.getcwd(), 'acceptance', 'labels', 'label_template.html'),
+        default_stylesheets=(os.path.join(os.getcwd(), 'acceptance', 'labels', 'style.css'),),
+        encoding='utf-8'
+    )
+
+    quantity = data.pop('quantity')
+
+    records = [dict(**data, current_date=datetime.now().strftime('YY/MM/DD')) for _ in range(int(quantity))]
+
+    temp = os.path.join(os.getcwd(), 'media', 'documents', 'temp')
+
+    file_path = os.path.join(temp, 'labels.pdf')
+
+    label_writer.write_labels(
+        records,
+        target=file_path,
+    )
+
+    with open(file_path, 'rb') as f:
+        document = Document.objects.create(
+            title='pdf',
+            document=File(f, name=os.path.basename(f.name))
+        )
+
+    return document.document.url
+
+
+def _get_total_quantity(sizes: dict, product_size: str) -> int:
+    total = 0
+
+    for size in sizes:
+        if size['origName'] == product_size:
+            for stock in size['stocks']:
+                total += stock['qty']
+
+    return total
+
+
+def _get_products():
+    product_ids = [product.article for product in Product.objects.all()]
+
+    valid_wb_ids = []
+
+    for product_id in product_ids:
+        try:
+            valid_wb_ids.append(int(product_id))
+        except ValueError:
+            pass
+
+    valid_wb_ids = list(map(str, valid_wb_ids))
+
+    return fetch_leftovers(valid_wb_ids)['data']['products']
+
+
+def update_leftovers():
+    products = _get_products()
+
+    for product in products:
+        russian_products = Product.objects.filter(article=product['id'])
+
+        for p in russian_products:
+            p.total_left = _get_total_quantity(product['sizes'], p.size)
+            p.save()
+
+
+def update_colors():
+    products = _get_products()
+
+    for product in products:
+        russian_products = Product.objects.filter(article=product['id'])
+
+        for p in russian_products:
+            p.color = ', '.join([color['name'] for color in product['colors']])
+            p.save()
