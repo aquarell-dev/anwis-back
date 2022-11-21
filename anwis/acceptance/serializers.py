9a46470f971ba6fe9e71aef81c40b87a98671c9d
Aquarell <aquarell.dev@gmail.com>
2022-11-21 12:14:28 +0300

    serailzers

--- anwis/acceptance/serializers.py
+++ anwis/acceptance/serializers.py
@@ -1,15 +1,7 @@
 from rest_framework import serializers
 
-from acceptance.models import Acceptance, StaffMember, AcceptanceCategory, Product
+from acceptance.models import Acceptance, StaffMember, AcceptanceCategory, Product, ProductSpecification
 from china.serializer import ProductQuantityDetailedSerializer
-
-
-class AcceptanceListSerializer(serializers.ModelSerializer):
-    products = ProductQuantityDetailedSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Acceptance
-        fields = '__all__'
 
 
 class AcceptanceCreateSerializer(serializers.ModelSerializer):
@@ -31,6 +23,38 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field='category', read_only=True)
+    photo = serializers.SerializerMethodField()
+    photo_id = serializers.SerializerMethodField(read_only=True)
+
+    def get_photo(self, obj):
+        request = self.context.get('request')
+
+        if obj.photo:
+            return request.build_absolute_uri(obj.photo.photo.url)
+
+    def get_photo_id(self, obj: Product):
+        if obj.photo:
+            return obj.photo.id
+
     class Meta:
         model = Product
+        fields = [
+            field.name for field in Product._meta.get_fields() if field.name not in ['productspecification']
+        ] + ['photo_id']
+
+
+class ProductSpecificationDetailedSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
+    class Meta:
+        fields = '__all__'
+        model = ProductSpecification
+
+
+class AcceptanceListSerializer(serializers.ModelSerializer):
+    products = ProductSpecificationDetailedSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Acceptance
         fields = '__all__'
