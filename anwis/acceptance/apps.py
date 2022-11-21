10b81ebf5cf61a324bed4f24a7b0361239f6a6f5
Aquarell <aquarell.dev@gmail.com>
2022-11-21 11:59:47 +0300

    added fetching leftovers auto

--- anwis/acceptance/apps.py
+++ anwis/acceptance/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AcceptanceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'acceptance'
+
+    def ready(self):
+        from .updater import start
+        start()
